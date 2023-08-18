f43de7b0a2a52f114943c2c51eaf0b53dd1dfac6
Romamill <122994080+Romamill@users.noreply.github.com>
2023-08-18 09:06:57 +0300

    style components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Button from './Button/Button';
@@ -9,6 +10,24 @@ import axios from 'axios';
 
 
 const API_KEY = '37990722-3d7325777fa7dcec3ffe4a675';
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #f0f0f0;
+`;
+
+const ContentContainer = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  padding: 20px;
+  margin: 0 auto;
+  background-color: #ffffff;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
 
 const App = () => {
   const [query, setQuery] = useState('');
@@ -59,15 +78,19 @@ const App = () => {
 
   return (
     <div>
-      <Searchbar onSubmit={handleSearchSubmit} />
-      <ImageGallery images={images} onImageClick={openModal} />
-      {isLoading && <MyLoader />}
-      {images.length > 0 && !isLoading && (
-        <Button onClick={loadMoreImages} />
-      )}
-      {modalImage && (
-        <Modal image={modalImage} onClose={closeModal} />
-      )}
+      <AppContainer>
+      <ContentContainer>
+        <Searchbar onSubmit={handleSearchSubmit} />
+        <ImageGallery images={images} onImageClick={openModal} />
+        {isLoading && <MyLoader />}
+        {images.length > 0 && !isLoading && (
+          <Button onClick={loadMoreImages} />
+        )}
+        {modalImage && (
+          <Modal image={modalImage} onClose={closeModal} />
+        )}
+      </ContentContainer>
+    </AppContainer>
     </div>
   );
 };
