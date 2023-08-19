1174fb71f2a3563a3e83dcef3163ddd4853369bf
Romamill <122994080+Romamill@users.noreply.github.com>
2023-08-19 09:32:26 +0300

    update Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,35 +1,57 @@
 import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 100;
+`;
+
+const ModalContent = styled.div`
+  background-color: #fff;
+`;
+
+const ModalImage = styled.img`
+  max-width: 100%;
+  max-height: 80vh;
+  object-fit: contain;
+`;
 
 const Modal = ({ image, onClose }) => {
   useEffect(() => {
-    const handleKeyPress = e => {
-      if (e.code === 'Escape') {
+    const handleKeyDown = event => {
+      if (event.key === 'Escape') {
         onClose();
       }
     };
 
-    window.addEventListener('keydown', handleKeyPress);
+    window.addEventListener('keydown', handleKeyDown);
 
     return () => {
-      window.removeEventListener('keydown', handleKeyPress);
+      window.removeEventListener('keydown', handleKeyDown);
     };
   }, [onClose]);
 
-  return (
-    <div className="overlay" onClick={onClose}>
-      <div className="modal">
-        <img src={image.largeImageURL} alt="" />
-      </div>
-    </div>
-  );
-};
+  const handleClickOutside = event => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
 
-Modal.propTypes = {
-  image: PropTypes.shape({
-    largeImageURL: PropTypes.string.isRequired,
-  }).isRequired,
-  onClose: PropTypes.func.isRequired,
+  return (
+    <ModalOverlay onClick={handleClickOutside}>
+      <ModalContent>
+        <ModalImage src={image.largeImageURL} alt={image.tags} />
+      </ModalContent>
+    </ModalOverlay>
+  );
 };
 
 export default Modal;
